1637ad405422343dd161bee2ce12b2f6a7d3a390
hiulit <hiulit@gmail.com>
2019-07-05 14:52:33 +0200

    Added collions layers and masks, and modes

--- Explosion.gd
+++ Explosion.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+export(PackedScene) var lala
 var obj_scene = load("res://box.tscn")
 
 var obj = []
@@ -13,17 +14,24 @@ var obj_hframes
 var frames = 0
 var sprite_name = "sprite"
 var collision_name = "collision"
+var destructible_collision_layers = 16
+var destructible_collision_masks = 1 + 16
 
 var drops = []
 
-var speed = 250
-var velocity = Vector2()
+var impulse = 500
 
 var detonate
+var timer = 0
+var max_time = 500
 
 func _ready():
-	get_node(sprite_name).vframes = 8
-	get_node(sprite_name).hframes = 8
+	print(get_path())
+	print("lala: ", lala)
+	print("obj_scene: ", obj_scene)
+
+	get_node(sprite_name).vframes = 4
+	get_node(sprite_name).hframes = 4
 	obj_vframes = get_node(sprite_name).vframes
 	obj_hframes = get_node(sprite_name).hframes
 
@@ -50,9 +58,14 @@ func _ready():
 #	print("object's collision_position: ", obj_collision_position)
 
 	for n in range(obj_vframes * obj_hframes):
-		obj.append(obj_scene.instance())
+		obj.append(lala.instance())
+		
+#		obj[n].set_collision_layer(destructible_collision_layers)
+#		obj[n].set_collision_mask(destructible_collision_masks)
 
-#		obj[n].set_mode(1) # static mode
+		obj[n].set_mode(MODE_STATIC) # static mode
+#		obj[n].get_node(collision_name).disabled = true
+		
 #		obj[n].set_use_custom_integrator(true)
 #		obj[n].set_sleeping(true)
 
@@ -71,19 +84,63 @@ func _ready():
 				y * (obj_width / obj_hframes) - obj_offset.x + obj_collision_extents.x + position.x,\
 				x * (obj_height / obj_vframes) - obj_offset.y + obj_collision_extents.y + position.y)
 
-			print("object ", frames, " position: ", obj[frames].position)
+#			print("object ", frames, " position: ", obj[frames].position)
 			frames += 1
 
 	call_deferred("add_children")
 
 
-#func _physics_process(delta):
+func _physics_process(delta):
+	if detonate:
+		for i in range(get_parent().get_child_count()):
+			var child = get_parent().get_child(i)
+			child.set_collision_layer(destructible_collision_layers)
+			child.set_collision_mask(destructible_collision_masks)
+			child.set_mode(MODE_RIGID)
+
+		timer += int(delta * 60)
+ 
+		if timer >= max_time:
+			for i in range(get_parent().get_child_count()):
+				var child = get_parent().get_child(i)
+				child.set_mode(MODE_STATIC)
+				child.get_node(collision_name).disabled = true
+
+			timer = 0
+			detonate = false
+
+#			for i in range(get_parent().get_child_count()):
+#				var child = get_parent().get_child(i)
+#				print("sleeping: ", child.sleeping)
+#
+#				if child.get_linear_velocity().x and child.get_linear_velocity().y < 0.5:
+#					child.set_mode(MODE_STATIC)
+#					child.get_node(collision_name).disabled = true
+#				else:
+#					print(child.get_linear_velocity())
+				
+
+#	if detonate:
+#		timer += delta * 60
+#		print(timer)
+#
+#	if timer >= max_time:
+#		detonate = false
+#		print("exceed time: ", max_time)
+#		for i in range(get_parent().get_child_count()):
+#			var child = get_parent().get_child(i)
+#			if child.get_linear_velocity() != Vector2(0, 0):
+#				print(child.get_linear_velocity().x)
+#				child.set_mode(MODE_STATIC)
+#				child.get_node(collision_name).disabled = true
+
 #	if Input.is_key_pressed(KEY_Q):
 #		detonate = true
 
 
 func _integrate_forces(state):
-	explosion()
+	if Input.is_key_pressed(KEY_Q):
+		explosion()
 
 
 func add_children():
@@ -95,23 +152,18 @@ func add_children():
 
 
 func explosion():
-	if Input.is_key_pressed(KEY_Q):
-		for i in range(get_parent().get_child_count()):
-			var child = get_parent().get_child(i)
-			
-			var child_scale = rand_range(0.5, 1.5)
-			child.get_node(sprite_name).scale = Vector2(child_scale, child_scale)
-			child.get_node(collision_name).scale = Vector2(child_scale, child_scale)
-			
-			var child_gravity_scale = rand_range(1, 2)
-			child.gravity_scale = child_gravity_scale
+	detonate = true
 
-#			child.set_use_custom_integrator(false)
-			child.mass = rand_range(1, 50)
-			child.apply_torque_impulse(100)
-			child.apply_impulse(Vector2(0, 0), Vector2(rand_range(-250, 250), rand_range(0, -100)))
+	for i in range(get_parent().get_child_count()):
+		var child = get_parent().get_child(i)
 
-#			child.set_mode(MODE_STATIC)
+		var child_scale = rand_range(0.5, 1.5)
+		child.get_node(sprite_name).scale = Vector2(child_scale, child_scale)
+		child.get_node(collision_name).scale = Vector2(child_scale, child_scale)
 
-			child.get_node(collision_name).disabled = true
-#			print(child.get_node(collision_name).disabled)
+		var child_gravity_scale = rand_range(1, 5)
+		child.gravity_scale = child_gravity_scale
+
+		child.mass = rand_range(1, 50)
+		child.apply_torque_impulse(100)
+		child.apply_impulse(Vector2(0, 0), Vector2(rand_range(-impulse, impulse), rand_range(-(impulse / 2), -impulse)))
