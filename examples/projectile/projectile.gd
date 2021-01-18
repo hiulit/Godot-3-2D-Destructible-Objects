91df7e457a80f9fa20daaa5ade25a8d9b5cf7604
hiulit <hiulit@gmail.com>
2021-01-18 11:38:46 +0100

    Update projectile.gd

--- examples/projectile/projectile.gd
+++ examples/projectile/projectile.gd
@@ -11,9 +11,7 @@ var only_shock_wave = false
 var debug_mode = false
 var remove = false
 
-
 func _ready():
-#	Engine.time_scale = 0.3
 	connect("body_entered", self, "collide")
 
 
@@ -37,14 +35,16 @@ func collide(body):
 				# Reset the destructible and collapsible blocks array.
 				body.object.destructible_blocks.resize(0)
 				body.object.collapsible_blocks.resize(0)
-	
+
 				for block in body.object.blocks_container.get_children():
 					if only_shock_wave:
 						if global_position.distance_to(block.global_position) < shock_wave_radius * shock_wave_threshold:
-							if debug_mode: block.modulate = Color("#00ff00")
+							if debug_mode:
+								block.modulate = Color("#00ff00")
 							body.object.destructible_blocks.append(block)
 						elif global_position.distance_to(block.global_position) > shock_wave_radius * shock_wave_threshold and global_position.distance_to(block.global_position) < shock_wave_radius:
-							if debug_mode: block.modulate = Color("#ff0000")
+							if debug_mode:
+								block.modulate = Color("#ff0000")
 							body.object.collapsible_blocks.append(block)
 					else:
 						if global_position.distance_to(block.global_position) < shock_wave_radius:
@@ -52,10 +52,10 @@ func collide(body):
 						else:
 							body.object.collapsible_blocks.append(block)
 
-				# Tell the object to detonate ans collapse.
-				if not debug_mode: body.object.detonate = true
-				if not debug_mode: body.object.collapse = true
-
+				# Tell the object to detonate and collapse.
+				if not debug_mode:
+					body.object.detonate = true
+					body.object.collapse = true
 
 			if destroy and not collapse:
 				# Reset the destructible blocks array.
@@ -65,19 +65,21 @@ func collide(body):
 					# Get only the blocks that are inside the shock wave radius.
 					if only_shock_wave:
 						if global_position.distance_to(block.global_position) < shock_wave_radius:
-							if debug_mode: block.modulate = Color("#ff0000")
+							if debug_mode:
+								block.modulate = Color("#ff0000")
 							body.object.destructible_blocks.append(block)
 					# Get all the blocks.
 					else:
 						body.object.destructible_blocks.append(block)
 
 				# Tell the object to detonate.
-				if not debug_mode: body.object.detonate = true
+				if not debug_mode:
+					body.object.detonate = true
 
 			if collapse and not destroy:
 				# Reset the collapsible blocks array.
 				body.object.collapsible_blocks.resize(0)
-	
+
 				# Populate the collapsible blocks array.
 				for block in body.object.blocks_container.get_children():
 					# Get only the blocks that are inside the shock wave radius.
@@ -90,14 +92,15 @@ func collide(body):
 						body.object.collapsible_blocks.append(block)
 
 				# Tell the object to collapse.
-				if not debug_mode: body.object.collapse = true
-
+				if not debug_mode:
+					body.object.collapse = true
 
 			if debug_mode and only_shock_wave:
 				# Set the object to static mode and remove
 				call_deferred("set_mode", MODE_STATIC)
-				body.remove_cover_sprite()
+#				body.remove_cover_sprite()
 				body.position = Vector2(-9999, -9999)
+
 				return
 
 		# Or free the node when colliding if you don't need it anymore.
